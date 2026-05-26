7ea079eab179a9a737972db5ec847d5b451f6f0b
Santiago Aburto <santiago123456emilio@gmail.com>
2026-05-26 17:31:21 -0400

    Add methods to CategoriaRepository for category queries

--- categoria/src/main/java/com/example/categoria/repository/CategoriaRepository.java
+++ categoria/src/main/java/com/example/categoria/repository/CategoriaRepository.java
@@ -2,6 +2,9 @@ package com.example.categoria.repository;
 import com.example.categoria.model.Categoria;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+
 public interface CategoriaRepository extends MongoRepository<Categoria, String>{
-    
+    Categoria findByNombreCategoria(String nombreCategoria);
+    boolean existsByNombreCategoria(String nombreCategoria);
+
 }
