6b9984de14fe99874f25be74ed90d597d6b5ef87
SB- Alumno <SB-Alumno@duoc.acad>
2026-06-01 11:14:14 -0400

    agregado de pelicula

---
+++ Pelicula/src/main/java/com/example/Pelicula/PeliculaApplication.java
@@ -0,0 +1,15 @@
+package com.example.Pelicula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PeliculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculaApplication.class, args);
+	}
+
+}
