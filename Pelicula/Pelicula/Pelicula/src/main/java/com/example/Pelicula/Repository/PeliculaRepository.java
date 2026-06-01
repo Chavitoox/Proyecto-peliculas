b50769c879d0b8e90b154697a86d085295e1446d
Santiago Aburto <santiago123456emilio@gmail.com>
2026-06-01 11:10:49 -0400

    Delete Pelicula/Pelicula/Pelicula directory

--- Pelicula/Pelicula/Pelicula/src/main/java/com/example/Pelicula/Repository/PeliculaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Pelicula.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.Pelicula.Model.Pelicula;
-
-import java.util.List;
-public interface PeliculaRepository extends JpaRepository <Pelicula ,Long>{
-    List<Pelicula> findByNombre(String nombre);
-}
- 
