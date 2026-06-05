f6988ad6b07cdd05af29bbbecadf4de91e4d81e5
Chavitoox <santiago123456emilio@gmail.com>
2026-06-05 19:34:35 -0400

    Fix: Reset completo de cache para forzar minusculas en GitHub

--- pelicula/src/main/java/com/example/pelicula/Exception/RecursoNoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package com.example.pelicula.Exception;
-
-public class RecursoNoEncontradoException extends RuntimeException{
-    public RecursoNoEncontradoException(String mensaje){
-        super(mensaje);
-    }
-}
-
