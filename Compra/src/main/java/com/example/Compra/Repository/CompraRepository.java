101cc9bd4c680fb4f4d561ca05fb4e9a4b22a4fb
MatiasC418 <ma.pizarroz@duocuc.cl>
2026-06-16 01:53:51 -0400

    Delete Compra directory

--- Compra/src/main/java/com/example/Compra/Repository/CompraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Compra.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Compra.Model.Compra;
-
-public interface CompraRepository extends JpaRepository<Compra, Long>{
-    
-}
