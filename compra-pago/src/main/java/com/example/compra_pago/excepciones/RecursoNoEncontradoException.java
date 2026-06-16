084797cd03e2f3ea4586746cdda2c1eda3ded9d9
MatiasC418 <ma.pizarroz@duocuc.cl>
2026-06-16 01:54:08 -0400

    Delete compra-pago directory

--- compra-pago/src/main/java/com/example/compra_pago/excepciones/RecursoNoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-package com.example.compra_pago.excepciones;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-    
-    public RecursoNoEncontradoException(String mensaje) {
-
-        super(mensaje);
-    }
-}
